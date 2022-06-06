7cb82c8950796c0fdd964fd3c33abb3ca9711b4a
lucascarcuschinski <77592478+lucascarcuschinski@users.noreply.github.com>
2022-06-06 16:57:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,26 @@ function swapiGet(param) {
 
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    var buscarPeople = document.getElementById("buscarPeople");
+  
+    buscarPeople.addEventListener("click", function () {
+      var idPeople = $("#idPeople").val();
+  
+      var myHEADER = "https://swapi.dev/api/people/" + idPeople + "";
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+          var res = JSON.parse(this.response);
+          document.getElementById("name").value = res.name;
+          document.getElementById("height").value = res.height;
+          document.getElementById("birth_year").value = res.birth_year;
+        }
+      };
+  
+      xmlhttp.open("GET", myHEADER);
+  
+      xmlhttp.send();
+      console.log("acabou");
+    });
+  });
